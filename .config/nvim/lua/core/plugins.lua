1934878ca14ae8f7b01013780a231a873f90eaf4
Paulo Henrique <paulo13579691@gmail.com>
2023-04-24 23:45:14 -0300

    add vim-tex plugin

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
     tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use 'lervag/vimtex'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
